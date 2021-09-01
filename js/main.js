2cad7bf76d20e3d01ae230cd86a4566ed617da61
Paloma <paloma.arevalog@gmail.com>
2021-09-01 10:43:40 +0200

    function handleClick and countClick done

--- js/main.js
+++ js/main.js
@@ -2,19 +2,36 @@
 
 const button = document.querySelector(".js_button");
 const input = document.querySelector(".js_input");
-const count = document.querySelector("js_count");
-const clue = document.querySelector("js_clue");
+const count = document.querySelector(".js_count");
+const clue = document.querySelector(".js_clue");
 const randomNumber = getRandomNumber(100);
+
 let acc = 0;
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
+function countClick() {
+    acc += 1;
+    count.value = `Número de intentos:${acc}`;
+}
+
 function handleClickButton(event) {
     event.preventDefault();
-    const number = input.value;
-    console.log(number);
+    let number = parseInt(input.value);
+    console.log(`Has introducido el número ${number}`);
+    if (number < 1 || number > 100) {
+        clue.innerHTML = "El número debe estar entre 1 y 100";
+    } else if (number > randomNumber) {
+        clue.innerHTML = "Demasiado alto";
+    } else if (number < randomNumber) {
+        clue.innerHTML = "Demasiado bajo";
+    } else if (number === randomNumber) {
+        clue.innerHTML = "Has ganado campeona!!!";
+    }
+    countClick();
 }
-console.log(randomNumber);
+
+console.log(`El número a adivinar es ${randomNumber}`);
 button.addEventListener("click", handleClickButton);
