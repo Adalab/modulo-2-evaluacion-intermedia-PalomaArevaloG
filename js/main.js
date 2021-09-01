8162e06ad23e32f9683a2be9b607174b75caf928
Paloma <paloma.arevalog@gmail.com>
2021-09-01 09:47:24 +0200

    create const and randomnumber function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-'use strict'
+"use strict";
 
-const
+const button = document.querySelector(".js_button");
+const input = document.querySelector(".js_input");
+const count = document.querySelector("js_count");
+const clue = document.querySelector("js_clue");
+const randomNumber = getRandomNumber(100);
+let acc = 0;
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
